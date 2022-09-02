62b8680efcbf7b85ab32cdaf841dbf93c6f85e7f
unknown <valentinechima95@gmail.com>
2022-09-02 20:39:06 +0100

    update on services

---
+++ main.js
@@ -0,0 +1,8 @@
+// document.addEventListener('click', function handleClickOutsideMenu(event) {
+//     const menu = document.getElementById('menu');
+
+//     if (!menu.contains(event.target)) {
+//         menu.style.display = '';
+//         btn.style.display = 'yes';
+//     }
+// })
